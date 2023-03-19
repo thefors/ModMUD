2d09106a4cb49f171d7457a5b60818c3b1ffd27a
Anders Lundholm Fors <mr.fors@gmail.com>
2023-03-19 21:42:12 +0100

    added Room class

--- ModMUD/Program.cs
+++ ModMUD/Program.cs
@@ -2,6 +2,34 @@
 {
     internal class Program
     {
+        class Room
+        {
+            int Number { get; set; }
+            string Name { get; set; }
+            string Presentation { get; set; }
+            int North { get; set; }
+            int East { get; set; }
+            int South { get; set; }
+            int West { get; set; }
+
+            public Room(int number = 0,
+                string name = "Entrance",
+                string presentation = "You've entered an empty room.",
+                int north = -1,
+                int east = -1,
+                int south = -1,
+                int west = -1)
+            {
+                Number = number;
+                Name = name;
+                Presentation = presentation;
+                North = north;
+                East = east;
+                South = south;
+                West = west;
+            }
+        }
+
         static void Main(string[] args)
         {
             string command;
